3c42ac5b01342da91d8c827b432f3bef3ca3295d
Nasse1234 <38816179+Nasse1234@users.noreply.github.com>
2018-09-14 16:11:50 +0300

    Initial commit

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector2(10, Random.Range(-6, 6));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
