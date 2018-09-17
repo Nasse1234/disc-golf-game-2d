7db0f86827b6870c224ac584e0b40ca8aac0e3dc
Nasse1234 <38816179+Nasse1234@users.noreply.github.com>
2018-09-17 16:36:20 +0300

    2

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public bool discInBasket = false;
+    private Vector2 velocity;
+    private Vector3 startpos;
+    private Vector3 endpos;
+    Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        startpos = new Vector3(transform.position.x, transform.localPosition.y, transform.localPosition.z);
+        endpos = new Vector3(transform.position.x + 40, transform.localPosition.y, transform.localPosition.z);
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(discInBasket == true)
+        {
+
+            cameraMove(); 
+        }
+	}
+    void cameraMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endpos, 10 * Time.deltaTime);
+        
+    }
+}
